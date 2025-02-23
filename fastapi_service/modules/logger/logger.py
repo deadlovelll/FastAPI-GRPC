f1823bd6dc4b7daab4b123ede8ac184cde66cf2f
deadlovelll <playboicartina@gmail.com>
2025-02-23 17:11:43 +0300

    added docstrings for logger

--- fastapi_service/modules/logger/logger.py
+++ fastapi_service/modules/logger/logger.py
@@ -1,16 +1,26 @@
 import os
-
-import logging 
-import logging.handlers
+import logging
 import json
 import socket
 import logstash
 
+from logging import Logger
+
 class LoggerModule:
+    
+    """
+    A class to initialize and configure a logger with Logstash integration.
+
+    This module sets up a logger that sends structured logs to a Logstash server.
+    """
     
     def __init__ (
         self,
     ) -> None:
+        
+        """
+        Initializes the logger module by reading environment variables for Logstash configuration.
+        """
         
         self.host = os.getenv('LOGGER_HOST')
         self.port = os.getenv('LOGGER_PORT')
@@ -18,17 +28,40 @@ class LoggerModule:
         
     def logger_initialization (
         self,
-    ) -> None:
+    ) -> Logger:
+        
+        """
+        Initializes and configures the logger with a Logstash handler.
+
+        The logger will send structured log messages in JSON format to a Logstash server.
+
+        Returns:
+            Logger: A configured logger instance.
+        """
         
         logger = logging.getLogger('fastapi-logger')
         logger.setLevel(logging.INFO)
 
         class CustomLogstashFormatter(logging.Formatter):
             
+            """
+            Custom formatter for structuring log messages in JSON format.
+            """
+            
             def format (
                 self, 
                 record,
             ) -> bytes:
+                
+                """
+                Formats log records into a structured JSON format.
+
+                Args:
+                    record (LogRecord): The log record to be formatted.
+
+                Returns:
+                    bytes: The formatted log message in JSON.
+                """
                 
                 log_record = {
                     'message': record.getMessage(),

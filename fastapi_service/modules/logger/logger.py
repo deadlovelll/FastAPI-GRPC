e838a26beb835453197d9b181924b088a384b2d2
deadlovelll <playboicartina@gmail.com>
2025-02-23 16:57:36 +0300

    updated queue and logger

--- fastapi_service/modules/logger/logger.py
+++ fastapi_service/modules/logger/logger.py
@@ -1,5 +1,4 @@
 import os
-from dotenv import load_dotenv
 
 import logging 
 import logging.handlers
@@ -7,25 +6,30 @@ import json
 import socket
 import logstash
 
-load_dotenv()
-
-from logging.handlers import TimedRotatingFileHandler
-
-class LoggerModel:
+class LoggerModule:
     
-    def __init__(self) -> None:
+    def __init__ (
+        self,
+    ) -> None:
         
         self.host = os.getenv('LOGGER_HOST')
         self.port = os.getenv('LOGGER_PORT')
         self.version = os.getenv('LOGGER_VERSION')
         
-    def logger_initialization(self):
+    def logger_initialization (
+        self,
+    ) -> None:
+        
         logger = logging.getLogger('fastapi-logger')
         logger.setLevel(logging.INFO)
 
         class CustomLogstashFormatter(logging.Formatter):
             
-            def format(self, record):
+            def format (
+                self, 
+                record,
+            ) -> bytes:
+                
                 log_record = {
                     'message': record.getMessage(),
                     'level': record.levelname,
@@ -33,14 +37,14 @@ class LoggerModel:
                     'host': socket.gethostname(),
                     'method': record.funcName,  
                     'filename': record.filename,  
-                    'line_number': record.lineno
+                    'line_number': record.lineno,
                 }
                 return json.dumps(log_record).encode('utf-8')  
 
-        logstash_handler = logstash.LogstashHandler(
+        logstash_handler = logstash.LogstashHandler (
             host=self.host, 
             port=int(self.port),         
-            version=int(self.version)
+            version=int(self.version),
         )
         
         logstash_handler.setFormatter(CustomLogstashFormatter())

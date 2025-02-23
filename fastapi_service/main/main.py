4bd6d915017e7820e78994058fd88283640049d8
deadlovelll <playboicartina@gmail.com>
2025-02-23 12:50:12 +0300

    refactored main.py module

--- fastapi_service/main/main.py
+++ fastapi_service/main/main.py
@@ -1,133 +1,54 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
 import uvicorn
-import grpc
+from fastapi import FastAPI
 
-from fastapi_service.modules.database.pool_controller.database_controller import DatabasePoolController
-from fastapi_service.modules.database.model.database import Database
-from fastapi_service.modules.logger.logger import LoggerModel
-from modules.base_controller import BaseController
+from fastapi_service.routers.books import books
 
-from schemas.book.book import Book
-from schemas.book_response.book_response import BookResponse
-from schemas.books_response.books_response import BooksResponse
-
-from pydantic import BaseModel
-
-Logger = LoggerModel()
-logger = Logger.logger_initialization()
-
-PoolController = DatabasePoolController(logger=logger)
-
-BaseController.initialize(db=PoolController.get_db(), logger=logger)
-
-GetBookController = GetController()
-PostBookController = PostController()
-PatchBookController = PatchController()
-DeleteBookController = DeleteController()
-
-app = FastAPI()
-
-
-@app.on_event('startup')
-async def startup_event():
+from fastapi_service.lifecycle_events.startup_events.startup_handler import StartupHandler
+from fastapi_service.lifecycle_events.shutdown_events.shutdown_handler import ShutdownHandler
     
-   await PoolController.startup_event()
+class AppFactory:
     
-@app.on_event('shutdown')
-async def shutdown_event():
+    def __init__ (
+        self,
+    ) -> None:
+        
+        self.startup_handler = StartupHandler()
+        self.shutdown_handler = ShutdownHandler()
+        
+        self.fastapi_app = FastAPI()
+        
+    def __setup_lifecycle_handlers (
+        self,
+    ) -> None:
+        
+        @self.fastapi_app.on_event('startup')
+        async def on_startup():
+            await self.startup_handler.handle_startup()
+            
+        @self.fastapi_app.on_event('shutdown')
+        async def on_shutdown():
+            self.shutdown_handler.handle_shutdown()
     
-    await PoolController.shutdown_event()
-
-@app.get('/')
-async def initial_func():
-    return JSONResponse({'hello':'world!'})
-
-@app.get (
-    '/get-all-books', 
-    response_model=BooksResponse, 
-    summary="Get all books", 
-    description="Retrieve a list of all books in the database."
-)
-async def get_all_books (
-    token: str,
-) -> JSONResponse:
+    def __include_routers (
+        self,
+    ) -> None:
+        
+        self.fastapi_app.include_router(books.router)
     
-    return await GetBookController.get_all_books (
-        token,
-    )
-
-@app.post (
-    '/post-book', 
-    response_model=BookResponse,
-)
-async def post_book (
-    book: Book, 
-    token: str,
-) -> JSONResponse:
+    def create (
+        self,
+    ) -> FastAPI:
+        
+        self.__setup_lifecycle_handlers()
+        self.__include_routers()
+        return self.fastapi_app
     
-    return await PostBookController.create_book (
-        book.book_name, 
-        book.book_author, 
-        token
-    )
+def get_app() -> FastAPI:
+    """Helper function to return an instance of the app."""
+    factory = AppFactory()
+    return factory.create()
 
-@app.patch (
-    '/edit-book', 
-    response_model=BookResponse,
-)
-async def edit_book (
-    book_id: int, 
-    book: Book, 
-    token: str
-) -> JSONResponse:
-    
-    return await PatchBookController.editbook (
-        book_id, 
-        book.book_name, 
-        book.book_author, 
-        token,
-    )
+app = get_app()
 
-@app.get (
-    '/get-all-books', 
-    response_model=BooksResponse,
-)
-async def get_all_books (
-    token: str,
-) -> JSONResponse:
-    
-    return await GetBookController.get_all_books (
-        token,
-    )
-
-@app.get (
-    '/get-book-{book_id}', 
-    response_model=BookResponse
-)
-async def get_book_by_id (
-    book_id: int, 
-    token: str,
-) -> JSONResponse:
-    
-    return await GetBookController.get_book_by_id (
-        book_id, 
-        token,
-    )
-
-@app.delete (
-    '/delete-book', 
-    response_model=BookResponse,
-)
-async def delete_book (
-    book_id: int, 
-    token: str,
-) -> JSONResponse:
-    
-    return await DeleteBookController.delete_book (
-        book_id, 
-        token,
-    )
-
-if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8100)
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8100)

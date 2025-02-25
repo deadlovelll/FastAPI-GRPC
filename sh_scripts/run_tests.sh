ba02569ba7a9bcd7702722bc218e5b181989da46
deadlovelll <playboicartina@gmail.com>
2025-02-25 23:37:12 +0300

    updated sh script

--- sh_scripts/run_tests.sh
+++ sh_scripts/run_tests.sh
@@ -1,5 +1,5 @@
-coverage run -m unittest discover -s django_service/base/tests -p "test*.py"
-coverage run -m unittest discover -s fastapi_service/tests -p "test*.py"
-coverage run -m unittest discover -s grpc_service/tests -p "test*.py"
+coverage run --source='./django_service' django_service/manage.py test base                                                                        
+coverage run --append -m unittest discover -s fastapi_service/tests -p "test*.py"
+coverage run --append -m unittest discover -s grpc_service/tests -p "test*.py"
 
 coverage report -m
